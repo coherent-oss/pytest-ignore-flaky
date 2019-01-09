2ecb9f71ba11c3162d17f44c3eb3fd9bfe08c496
Marcos Alfredo <marcos.alfredo@gmail.com>
2019-01-09 18:27:03 +0000

    update plugin version and minimum pytest version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(os.path.join(os.path.dirname(__file__), 'README.rst'), 'r') as fp:
 
 setup(name = 'pytest-ignore-flaky',
       description = 'ignore failures from flaky tests (pytest plugin)',
-      version = '0.1.1',
+      version = '0.1.2',
       license = 'MIT',
       author = 'Eduardo Naufel Schettino',
       author_email = 'schettino72@gmail.com',
@@ -29,7 +29,7 @@ setup(name = 'pytest-ignore-flaky',
                      ],
       py_modules = ['pytest_ignore_flaky'],
       install_requires = [
-          'pytest>=2.7',
+          'pytest>=3.7',
       ],
       entry_points = {
         'pytest11': ['pytest_ignore_flaky = pytest_ignore_flaky'],
